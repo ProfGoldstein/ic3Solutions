542514b1e571b5d207676511c8a4e0c8b7883531
ProfGoldstein <igoldstein@siena.edu>
2023-02-19 16:31:07 -0500

    initial upload

--- PrefixSumListIterator.java
+++ PrefixSumListIterator.java
@@ -10,32 +10,32 @@ import java.util.*;
 
 public class PrefixSumListIterator implements java.util.Iterator<Integer> {
 
-    private List<Integer> list;  		// The list of integers
+    private List<Integer> list;   // The list of integers
     private Iterator<Integer> listIterator;	//The list's iterator
-    private int prefixSum;				// The sum of the elements in the array
+    private int prefixSum;        // The sum of the elements in the array
 
     // Constructor
     /**
-	* The PrefixSumListIterator constructor
-	* Takes a list and initializes the iterator and prefix sum
-	* @param list    The list containing the values
-	*/
+    * The PrefixSumListIterator constructor
+    * Takes a list and initializes the iterator and prefix sum
+    * @param list    The list containing the values
+    */
     public PrefixSumListIterator(List<Integer> list) {
-		this.list = list;		// Initializes the list we will be iterating
+        this.list = list;		// Initializes the list we will be iterating
         this.listIterator = list.listIterator(); 	// Initializes the iterator
-		this.prefixSum = 0;		// Initializes the running sum
+        this.prefixSum = 0;		// Initializes the running sum
     }
 
     /**
-	* Overrides the hasNext() method
-	* Checks to see if ther are any more elements in the list that we have
-	* not yet added to the prefix sum
+    * Overrides the hasNext() method
+    * Checks to see if ther are any more elements in the list that we have
+    * not yet added to the prefix sum
 	*
 	* @return True if there are more values, False if there are no more values
 	*/
     @Override
     public boolean hasNext() {
-	    return listIterator.hasNext();
+        return listIterator.hasNext();
     }
 
     /**
@@ -45,69 +45,70 @@ public class PrefixSumListIterator implements java.util.Iterator<Integer> {
 	*/
     @Override
     public Integer next() {
-		prefixSum += listIterator.next();	// Adds the value of the current 
-											// integer to the running sum
-		return prefixSum;
+        prefixSum += listIterator.next();   // Adds the value of the current 
+                                            // integer to the running sum
+        return prefixSum;
     }
 
     /**
     * main method to test the PrefixSumListIterator class.
-	*
+    *
     * @param args[0] size of the list to generate
     * @param args[1] range of random values
     */
     public static void main(String args[]) {
 
-		if (args.length != 2) {
-			System.err.println("Usage: java PrefixSumListIterator size range");
-			System.exit(1);
-		}
+        if (args.length != 2) {
+            System.err.println("Usage: java PrefixSumListIterator size range");
+            System.exit(1);
+        }
 
 	    // convert the command-line parameters to the numbers needed
-		int n = 0;
-		int range = 0;
-		try {
-			n = Integer.parseInt(args[0]);
-			range = Integer.parseInt(args[1]);
-		}
-		catch (NumberFormatException e) {
-			System.err.println(e);
-			System.exit(1);
-		}
+        int n = 0;
+        int range = 0;
+        try {
+            n = Integer.parseInt(args[0]);
+            range = Integer.parseInt(args[1]);
+        }
+        catch (NumberFormatException e) {
+            System.err.println(e);
+        System.exit(1);
+        }
 
 	    // create and populate three list, LinkedList, Vector, and Stack
-		java.util.Random r = new java.util.Random();
-		List<Integer> lList = new LinkedList<Integer>();
-		List<Integer> vList = new Vector<Integer>();
-		List<Integer> sList = new Stack<Integer>();
-		for (int i = 0; i < n; i++) {
-			int rNum = r.nextInt(range);
-			lList.add(rNum);
-			vList.add(rNum);
-			sList.add(rNum);
-		}
+        java.util.Random r = new java.util.Random();
+        List<Integer> lList = new LinkedList<Integer>();
+        List<Integer> vList = new Vector<Integer>();
+        List<Integer> sList = new Stack<Integer>();
+        for (int i = 0; i < n; i++) {
+            int rNum = r.nextInt(range);
+            lList.add(rNum);
+            vList.add(rNum);
+            sList.add(rNum);
+        }
 
         // Print out the list of numbers
-		System.out.println("Generated list: " + lList);
+        System.out.println("Generated list: " + lList);
 
         // Print out the LinkedList prefix sums
-		System.out.println("Prefix LinkedList sums:");
-		java.util.Iterator<Integer> linkedIterator = new PrefixSumListIterator(lList);
-		while (linkedIterator.hasNext()) {
-			System.out.println(linkedIterator.next());
-		}
+        System.out.println("Prefix LinkedList sums:");
+        java.util.Iterator<Integer> linkedIterator = new PrefixSumListIterator(lList);
+        while (linkedIterator.hasNext()) {
+            System.out.println(linkedIterator.next());
+        }
+		
         // Print out the Vector prefix sums		
-		System.out.println("Prefix Vector sums:");
-		java.util.Iterator<Integer> vectorIterator = new PrefixSumListIterator(vList);
-		while (vectorIterator.hasNext()) {
-			System.out.println(vectorIterator.next());
-		}	
+        System.out.println("Prefix Vector sums:");
+        java.util.Iterator<Integer> vectorIterator = new PrefixSumListIterator(vList);
+        while (vectorIterator.hasNext()) {
+            System.out.println(vectorIterator.next());
+        }	
 		
         // Print out the Stack prefix sums
-		System.out.println("Prefix Stack sums:");
-		java.util.Iterator<Integer> stackIterator = new PrefixSumListIterator(sList);
-		while (stackIterator.hasNext()) {
-			System.out.println(stackIterator.next());
-		}		
+        System.out.println("Prefix Stack sums:");
+        java.util.Iterator<Integer> stackIterator = new PrefixSumListIterator(sList);
+        while (stackIterator.hasNext()) {
+            System.out.println(stackIterator.next());
+        }		
     }
 }
